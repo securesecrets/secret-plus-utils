55f11e0e53c472073ddf1a9e0feca9a68777d592
hydropump3 <3ki2fiay@anonaddy.me>
2022-10-06 01:18:03 -0400

    adds benchmarking

--- packages/storage-plus/benches/serialization.rs
+++ packages/storage-plus/benches/serialization.rs
@@ -1,21 +1,33 @@
-use cosmwasm_std::{testing::MockStorage, Uint512};
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use better_secret_math::U256;
-use rand::Rng;
+use borsh::{BorshDeserialize, BorshSerialize};
+use cosmwasm_std::testing::MockStorage;
+use criterion::{black_box, criterion_group, Criterion};
+use secret_borsh_storage::Item as BorshItem;
 use serde::{Deserialize, Serialize};
-use std::mem;
-use std::time::Duration;
 
-use secret_storage_plus::{Item, Json, Bincode2, Serde};
+use secret_storage_plus::{Bincode2, Item, Json, Serde};
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize, PartialEq, Debug)]
 struct Config {
     pub owner: String,
+    pub address: String,
     pub max_tokens: i32,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+impl Config {
+    pub fn new() -> Self {
+        Config {
+            owner: "iaudnfuafbuwiafnawiuofngsgjknsjgnsogvnsgjnaigojanfasjknfakjnakfjanfkasfnafkjsafnajkfnajfanfka".to_string(),
+            address: "iaudnfuafbuwiafnawiuofngsgjknsjgnsogvnsgjnaigojanfasjknfakjnakfjanfkasfnafkjsafnajkfnajfanfka".to_string(),
+            max_tokens: i32::MAX,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize, PartialEq, Debug)]
 struct BigChungis {
+    pub owner: String,
+    pub address: String,
     pub b: [U256; 2],
     pub i: [U256; 2],
     pub g: [U256; 2],
@@ -24,15 +36,24 @@ struct BigChungis {
 
 impl BigChungis {
     pub fn big() -> Self {
-        BigChungis { b: [U256::MAX; 2], i: [U256::MAX; 2], g: [U256::MAX; 2], chungis: [U256::MAX; 10] }
+        BigChungis {
+            owner: "iaudnfuafbuwiafnawiuofngsgjknsjgnsogvnsgjnaigojanfasjknfakjnakfjanfkasfnafkjsafnajkfnajfanfka".to_string(),
+            address: "iaudnfuafbuwiafnawiuofngsgjknsjgnsogvnsgjnaigojanfasjknfakjnakfjanfkasfnafkjsafnajkfnajfanfka".to_string(),
+            b: [U256::MAX; 2],
+            i: [U256::MAX; 2],
+            g: [U256::MAX; 2],
+            chungis: [U256::MAX; 10],
+        }
     }
 }
 
 const CONFIG: Item<Config, Bincode2> = Item::new("blahblahbalh");
 const CONFIG_JSON: Item<Config, Json> = Item::new("blahblahbalh");
+const CONFIG_BORSH: BorshItem<Config> = BorshItem::new("adwadwa");
 
 const BIG: Item<BigChungis, Bincode2> = Item::new("blahblahbalh");
 const BIG_JSON: Item<BigChungis, Json> = Item::new("blahblahbalh");
+const BIG_BORSH: BorshItem<BigChungis> = BorshItem::new("adwadwa");
 
 fn big_save_and_load(item: Item<BigChungis, impl Serde>) {
     let mut store = MockStorage::new();
@@ -46,16 +67,37 @@ fn big_save_and_load(item: Item<BigChungis, impl Serde>) {
     assert_eq!(big, item.load(&store).unwrap());
 }
 
+fn borsh_big_save_and_load(item: BorshItem<BigChungis>) {
+    let mut store = MockStorage::new();
+
+    assert!(item.load(&store).is_err());
+    assert_eq!(item.may_load(&store).unwrap(), None);
+
+    let big = BigChungis::big();
+    item.save(&mut store, &big).unwrap();
+
+    assert_eq!(big, item.load(&store).unwrap());
+}
+
+fn borsh_save_and_load(item: BorshItem<Config>) {
+    let mut store = MockStorage::new();
+
+    assert!(item.load(&store).is_err());
+    assert_eq!(item.may_load(&store).unwrap(), None);
+
+    let cfg = Config::new();
+    item.save(&mut store, &cfg).unwrap();
+
+    assert_eq!(cfg, item.load(&store).unwrap());
+}
+
 fn save_and_load(item: Item<Config, impl Serde>) {
     let mut store = MockStorage::new();
 
     assert!(item.load(&store).is_err());
     assert_eq!(item.may_load(&store).unwrap(), None);
 
-    let cfg = Config {
-        owner: "admin".to_string(),
-        max_tokens: 1234,
-    };
+    let cfg = Config::new();
     item.save(&mut store, &cfg).unwrap();
 
     assert_eq!(cfg, item.load(&store).unwrap());
@@ -65,27 +107,27 @@ fn bench_bincode2_json(c: &mut Criterion) {
     let mut group = c.benchmark_group("Bincode vs Json");
 
     group.bench_function("bincode2 save & load", |b| {
-        b.iter(|| {
-            save_and_load(black_box(CONFIG))
-        } )
+        b.iter(|| save_and_load(black_box(CONFIG)))
     });
 
     group.bench_function("json save & load", |b| {
-        b.iter(|| {
-            save_and_load(black_box(CONFIG_JSON))
-        } )
+        b.iter(|| save_and_load(black_box(CONFIG_JSON)))
+    });
+
+    group.bench_function("borsh save & load", |b| {
+        b.iter(|| borsh_save_and_load(black_box(CONFIG_BORSH)))
     });
 
     group.bench_function("bincode2 big save & load", |b| {
-        b.iter(|| {
-            big_save_and_load(black_box(BIG))
-        } )
+        b.iter(|| big_save_and_load(black_box(BIG)))
     });
 
     group.bench_function("json big save & load", |b| {
-        b.iter(|| {
-            big_save_and_load(black_box(BIG_JSON))
-        } )
+        b.iter(|| big_save_and_load(black_box(BIG_JSON)))
+    });
+
+    group.bench_function("borsh big save & load", |b| {
+        b.iter(|| borsh_big_save_and_load(black_box(BIG_BORSH)))
     });
 
     group.finish();
