0b03aaa484a83a7c535ded0d1a0881f357bfe4b6
hydropump3 <3ki2fiay@anonaddy.me>
2022-10-26 16:34:16 -0400

    update secret

--- packages/secretcli/src/constants.rs
+++
@@ -1,4 +0,0 @@
-pub const SECRETCLI: &str = "secretd";
-
-pub const STORE_GAS: &str = "10000000";
-pub const GAS: &str = "800000";
--- packages/secretcli/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod cli_types;
-pub mod secretcli;
-pub mod utils;
-pub mod constants;
-#[cfg(feature = "helpers")]
-pub mod deployable;
